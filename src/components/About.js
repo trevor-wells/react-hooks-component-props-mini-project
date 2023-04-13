6b058d0956021a8fa754c9ecbc51fd2d158d4c47
trevor wells <104236134+trevor-wells@users.noreply.github.com>
2023-04-13 11:57:04 -0600

    final final commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-function About({image = "https://via.placeholder.com./215", about}){
+function About({image = "https://via.placeholder.com/215", about}){
     return(
         <aside>
             <img src={image} alt="blog logo"/>
